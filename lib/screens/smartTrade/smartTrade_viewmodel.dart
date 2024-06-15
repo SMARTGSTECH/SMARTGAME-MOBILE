9c152191efc08e5e74f7e5f97155d4ad9e0182de
Sixtus <6ixtus6@gmail.com>
2024-06-15 15:35:45 +0100

    started writting the getter funtion tosave changes

--- lib/screens/smartTrade/smartTrade_viewmodel.dart
+++ lib/screens/smartTrade/smartTrade_viewmodel.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:smartbet/socket/provider.dart';
 
 class SmartTradeProvider extends ChangeNotifier {
   List gameOption = ['\$30 - \$209', '\$10 - \$500', '\$30 - \$204', '\$30'];
@@ -12,10 +14,14 @@ class SmartTradeProvider extends ChangeNotifier {
     notifyListeners();
   }
 
-  List get gameOptions {
-    List dataVal = [];
-
-    return [];
+  List gameOptions(context, symbol) {
+    Map val = Provider.of<SocketProvider>(context, listen: false)
+        .smartTradeOptionValue;
+    double option1 = 0;
+    double option2 = 0;
+    double option3 = 0;
+    double option4 = 0;
+    return [option1, option2, option3, option4];
   }
 
   toggleOption(String option) {
