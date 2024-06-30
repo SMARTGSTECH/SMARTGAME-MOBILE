2e62af6ff02382f9ff175bbfe00c4ae20bd934d5
Sixtus <6ixtus6@gmail.com>
2024-06-30 14:51:24 +0100

    save refactore changes

--- lib/widget/bottomModal.dart
+++ lib/widget/bottomModal.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:provider/provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'package:smartbet/screens/home/provider.dart';
 import 'package:smartbet/utils/config/color.dart';
 import 'package:smartbet/walletConnect/provider.dart';
+import 'package:smartbet/widget/connectWallet.dart';
 
 class ReusableBottomModal extends StatelessWidget {
   @override
@@ -27,8 +29,8 @@ class ReusableBottomModal extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: List.generate(
                       model.chainIMG.length,
-                      (index) => walletcard(
-                          coinP.coinArray, model.chainIMG[index], model)),
+                      (index) => walletcard(coinP.coinArray,
+                          model.chainIMG[index], model, context)),
                 )),
           ),
         );
@@ -37,7 +39,7 @@ class ReusableBottomModal extends StatelessWidget {
   }
 }
 
-walletcard(List array, type, UserWeb3Provider walletnstance) {
+walletcard(List array, type, UserWeb3Provider walletnstance, contect) {
   return Card(
     color: ColorConfig.appBar.withOpacity(0.5),
     elevation: 1,
@@ -116,8 +118,48 @@ walletcard(List array, type, UserWeb3Provider walletnstance) {
                       .walletInstanceMap[type.toLowerCase()]!['isConnected']
                   ? ColorConfig.red
                   : ColorConfig.iconColor)
-          .onTap(
-              walletnstance.walletInstanceMap[type.toLowerCase()]!['method']),
+          .onTap(() {
+        type.toLowerCase() == "eth"
+            ? walletnstance.connectPartilcle()
+            : type.toLowerCase() == "sol"
+                ? walletnstance.adapter
+                    .authorize()
+                    .then((value) => walletnstance.output = value.toJson())
+                    .catchError((error) => walletnstance.output = error)
+                : showDialog(
+                    context: contect,
+                    barrierDismissible: true,
+                    builder: (BuildContext context) {
+                      return Dialog(
+                        //  shadowColor: ColorConfig.blue,
+                        // shape: CircleBorder(),
+                        backgroundColor: Colors.transparent,
+                        elevation: 10,
+                        child: Container(
+                          color: ColorConfig.white,
+                          child: QrImageView(
+                            backgroundColor: Colors.transparent,
+                            data: walletnstance.universalLink!,
+                            version: QrVersions.auto,
+                            //size: 120,
+                            gapless: false,
+                          ).onTap(() {
+                            print(walletnstance.connector.wallet!.account);
+                          }),
+                          height: 240.w,
+                          width: 240.w,
+                        ).cornerRadiusWithClipRRect(10.r),
+                      );
+                    },
+                  );
+        // walletnstance
+        //     .
+        //     // walletnstance.updateMap(
+        //     //     {"isConnected": false, "address": "", "base": "", "bnb": ""},
+        //     //     "ton");
+
+        //   //  print(walletnstance.ton);
+      }),
     ),
   );
 }
